a01d6b38bac055537de6238ac0dd73aa3a95580b
Sylvain Viollon <thefunny@gmail.com>
2008-08-23 19:06:52 +0000

    - Oups, bad copy. Put back the branch in branches.

--- sylvain-five-grok-static/src/grokcore/view/tests/static/staticispackage.py
+++
@@ -1,9 +0,0 @@
-"""
-It is an error for the 'static' directory to be a python package:
-
-  >>> import grokcore.view as grok
-  >>> grok.testing.grok('grokcore.view.tests.static.staticispackage_fixture')
-  Traceback (most recent call last):
-    ...
-  GrokError: The 'static' resource directory must not be a python package.
-"""
