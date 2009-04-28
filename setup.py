f5fc689f3a55a05841ae538f608a3d23650e0ecd
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2009-04-28 07:31:40 +0000

    Back to development: 1.7dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.view',
-    version = '1.6',
+    version = '1.7dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
