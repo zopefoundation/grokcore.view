2913ab445a26d4d3ca6647e26149bf92387fb2f1
Reinout van Rees <reinout@vanrees.org>
2009-07-04 14:41:12 +0000

    Back to development: 1.10dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.view',
-    version = '1.9',
+    version = '1.10dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
