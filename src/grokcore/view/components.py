c01675074d82df41b09133862e2417b18bbc6d5f
Christian Klinger <cklinger@novareto.de>
2009-09-15 16:44:44 +0000

    Explicit checking of the View's interface now allows IGrokBaseView as it has a namespace and a static methods.

--- src/grokcore/view/components.py
+++ src/grokcore/view/components.py
@@ -239,7 +239,7 @@ class PageTemplate(GrokTemplate):
         factory.macros = property(_get_macros)
 
     def render(self, view):
-        assert interfaces.IGrokView.providedBy(view)
+        assert interfaces.IGrokBaseView.providedBy(view)
         namespace = self.getNamespace(view)
         template = self._template
         namespace.update(template.pt_getContext())
