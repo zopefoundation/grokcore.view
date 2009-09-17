f57b97dbdae8fabef00bcb503ad29f2412364bed
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2009-09-17 14:22:59 +0000

    Move test fix on trunk to maintenance branch.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.view',
-    version = '1.12.1',
+    version = '1.12.2dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
