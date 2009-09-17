426da68dae00abd2ec0b1fa42cce23bde776f874
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2009-09-17 13:40:57 +0000

    Ugly windows compliance bugfix

--- src/grokcore/view/tests/directoryresource/directoryresources.py
+++ src/grokcore/view/tests/directoryresource/directoryresources.py
@@ -7,11 +7,11 @@
   >>> from zope.component import getAdapter
   >>> from zope.publisher.browser import TestRequest
   >>> resource_foo = getAdapter(TestRequest(), Interface, name='foo')
-  >>> resource_foo.context.path
+  >>> resource_foo.context.path.replace('\\\\', '/') # Windows compliance 
   '...grokcore/view/tests/directoryresource/directoryresources_fixture/foo'
 
   >>> resource_baz = getAdapter(TestRequest(), Interface, name='baz')
-  >>> resource_baz.context.path
+  >>> resource_baz.context.path.replace('\\\\', '/') # Windows compliance 
   '...grokcore/view/tests/directoryresource/directoryresources_fixture/bar/baz'
 
 """
