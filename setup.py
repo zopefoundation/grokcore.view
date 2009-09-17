ffe46a72d3c9176dc9f054cb02d2bf41a59e9cf9
Martijn Faassen <faassen@startifact.com>
2009-09-17 07:47:30 +0000

    Back to development: 1.13dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.view',
-    version = '1.12',
+    version = '1.13dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
