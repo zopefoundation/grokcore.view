e5c74db59d37a4879ab50ee0fd9ed0af0e071a47
Uli Fouquet <uli@gnufix.de>
2010-05-27 00:36:43 +0000

    Backport warnings-as-log-msgs from trunk.

--- src/grokcore/view/tests/view/dirtemplatesonly.py
+++ src/grokcore/view/tests/view/dirtemplatesonly.py
@@ -1,15 +1,15 @@
 """
 A template directory may only contain recognized template files::
 
-  >>> from grokcore.view.testing import warn, lastwarning
-  >>> import warnings
-  >>> saved_warn = warnings.warn
-  >>> warnings.warn = warn
+  >>> from zope.testing.loggingsupport import InstalledHandler
+  >>> handler = InstalledHandler('grokcore.view')
+  >>> handler.clear()
 
   >>> grok.testing.grok(__name__)
-  From grok.testing's warn():
-  ... UserWarning: File 'invalid.txt' has an unrecognized extension in
-  directory '...dirtemplatesonly_templates'...
+  >>> print handler
+  grokcore.view WARNING
+      File 'invalid.txt' has an unrecognized extension in directory
+      '...dirtemplatesonly_templates'
 
 Files ending with '.cache' are generated on the fly by some template
 engines. Although they provide no valid template filename extension,
@@ -18,15 +18,16 @@ they are ignored.
 There is a 'template' ``ignored.cache`` in our template dir, which
 emits no warning::
 
-  >>> 'ignored.cache' in lastwarning
+  >>> #'ignored.cache' in lastwarning
+  >>> 'ignored.cache' in handler.records[0].msg
   False
 
 The same applies to files and directories ending with '~' or starting
 with a dot ('.').
 
-Restore the warning machinery::
+Restore the logging machinery::
 
-  >>> warnings.warn = saved_warn
+  >>> handler.uninstall()
 
 """
 import grokcore.view as grok
--- src/grokcore/view/tests/view/shared_template_dir.py
+++ src/grokcore/view/tests/view/shared_template_dir.py
@@ -1,24 +1,32 @@
 """
-When modules share a template directory, templates that have not been associated with any view class of a given module issue a UserWarning:
+When modules share a template directory, templates that have not been
+associated with any view class of a given module issue a log message
+with level ``logging.WARNING`` to a ``grokcore.view`` logger:
 
-  >>> from grokcore.view.testing import warn
-  >>> import warnings
-  >>> saved_warn = warnings.warn
-  >>> warnings.warn = warn
+  >>> from zope.testing.loggingsupport import InstalledHandler
+  >>> handler = InstalledHandler('grokcore.view')
 
   >>> import shared_template_dir_fixture
 
+  >>> handler.clear()  # Make sure no old log msgs are stored...
   >>> grok.testing.grok(__name__)
-  From grok.testing's warn():
-  ...UserWarning: Found the following unassociated template(s) when grokking
-  'grokcore.view.tests.view.shared_template_dir': food, unassociated.  Define view classes inheriting from
-  grok.View to enable the template(s)...
-
+  >>> print handler
+  grokcore.view WARNING
+      Found the following unassociated template(s) when grokking
+      'grokcore.view.tests.view.shared_template_dir': food,
+      unassociated.  Define view classes inheriting from grok.View to
+      enable the template(s).
+  
+  >>> handler.clear()  # Make sure no old log msgs are stored...
   >>> grok.testing.grok(shared_template_dir_fixture.__name__)
-  From grok.testing's warn():
-  ...UserWarning: Found the following unassociated template(s) when grokking
-  'grokcore.view.tests.view.shared_template_dir_fixture': cavepainting, unassociated.  Define view classes inheriting from
-  grok.View to enable the template(s)...
+  >>> print handler
+  grokcore.view WARNING
+        Found the following unassociated template(s) when grokking
+      'grokcore.view.tests.view.shared_template_dir_fixture':
+      cavepainting, unassociated.  Define view classes inheriting from
+      grok.View to enable the template(s).
+
+  >>> handler.uninstall()
 
 """
 import grokcore.view as grok
