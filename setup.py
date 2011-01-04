eea1225e90332a34c7a8559755958a4deabcf696
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2011-01-04 14:27:21 +0000

    Preparing release 2.3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ publication_require = [
 
 setup(
     name='grokcore.view',
-    version='2.3dev',
+    version='2.3',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
