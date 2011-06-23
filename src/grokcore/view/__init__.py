252692b5435994fc76a9d7c82276bf4ac15120af
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-06-23 09:30:03 +0000

    add tests similar to those on grokcore.viewlet

--- src/grokcore/view/__init__.py
+++ src/grokcore/view/__init__.py
@@ -19,7 +19,7 @@ from grokcore.security import *
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
-from grokcore.view.components import View, ViewSupport
+from grokcore.view.components import View, ViewSupport, ContentProvider
 from grokcore.view.components import PageTemplate, PageTemplateFile
 from grokcore.view.interfaces import IGrokSecurityView
 from grokcore.view.components import DirectoryResource
