ab1105215d26d55b091a26e806d44f1710125291
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2012-12-11 11:08:25 +0000

    Preparing release 2.8

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ publication_require = [
 
 setup(
     name='grokcore.view',
-    version='2.8.dev0',
+    version='2.8',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
