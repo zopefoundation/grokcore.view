5cfcb3ad8f51dddd5f6cc527881035423a8f2617
Sylvain Viollon <thefunny@gmail.com>
2014-05-14 15:58:11 +0200

    Update test.

--- src/grokcore/view/tests/skin/notonlayer.py
+++ src/grokcore/view/tests/skin/notonlayer.py
@@ -8,11 +8,12 @@ registered as a skin:
   GrokError: The grok.skin() directive is used on interface
   'grokcore.view.tests.skin.notonlayer.NotALayer'. However,
   'grokcore.view.tests.skin.notonlayer.NotALayer' does not extend
-  IBrowserRequest which is required for interfaces that are used as
+  IRequest which is required for interfaces that are used as
   layers and are to be registered as a skin.
 """
 import grokcore.view as grok
 from zope.interface import Interface
+
 
 class NotALayer(Interface):
     grok.skin('not a layer')
