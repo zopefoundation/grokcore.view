a5391849b406691725fb514f3681b58d0317dbe7
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2014-05-15 11:48:43 +0200

    Back to development: 2.10

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ publication_require = [
 
 setup(
     name='grokcore.view',
-    version='2.9',
+    version='2.10.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
