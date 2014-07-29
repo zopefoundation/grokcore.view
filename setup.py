010cef040f744ea999fdc78e1e4fdabfcd5cd129
Sylvain Viollon <thefunny@gmail.com>
2014-07-29 11:43:19 +0200

    Preparing release 2.10.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ publication_require = [
 
 setup(
     name='grokcore.view',
-    version='2.11.dev0',
+    version='2.10.1',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
