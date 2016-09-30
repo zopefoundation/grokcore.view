326aef63d703ea45e1e6425919bc13028f22268b
Steffen Allner <sa@gocept.com>
2016-09-30 14:17:17 +0200

    PY2-PY3: Fix various import issues with relative imports.

--- src/grokcore/view/tests/view/shared_template_dir.py
+++ src/grokcore/view/tests/view/shared_template_dir.py
@@ -7,7 +7,7 @@ When modules share a template directory, templates that have not been associated
   >>> saved_warn = warnings.warn
   >>> warnings.warn = warn
 
-  >>> import shared_template_fixture 
+  >>> from grokcore.view.tests.view import shared_template_fixture
 
   >>> grok.testing.grok(shared_template_fixture.__name__)
   From grok.testing's warn():
