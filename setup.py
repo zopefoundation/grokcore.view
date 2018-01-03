d23cfe78549a70bdd4e1a40870b7d18e4d4312f3
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-03 16:25:28 +0100

    Back to development: 3.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ publication_require = [
 
 setup(
     name='grokcore.view',
-    version='3.0',
+    version='3.1.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
